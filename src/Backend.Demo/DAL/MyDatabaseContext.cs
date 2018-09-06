4563ad8766433305d749cedc7442afd7705ea1f3
pczerwenka <42975072+pczerwenka@users.noreply.github.com>
2018-09-06 14:18:50 -0600

    Backend database and entity

---
+++ src/Backend.Demo/DAL/MyDatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Demo.DAL
+{
+    internal class MyDatabaseContext : DbContext 
+    {
+        //Don't forget for the base it uses the name that we gave to our connection string in the web config file!!!
+        public MyDatabaseContext() : base("name=AdhocDb")
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
